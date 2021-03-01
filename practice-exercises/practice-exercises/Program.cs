d19f93ac8854dd8414122a0b2f8720e2beb0609a
Andaluzov <zhurbasi@gmail.com>
2021-03-02 00:03:05 +0200

    task1-Task3

--- practice-exercises/practice-exercises/Program.cs
+++ practice-exercises/practice-exercises/Program.cs
@@ -9,6 +9,8 @@ namespace practice_exercises
             Console.WriteLine("Hello World!");
             SayHello();
             Task1();
+            Task2();
+            Task3();
         }
         static void SayHello()
         {
@@ -50,6 +52,70 @@ namespace practice_exercises
             { 
                 Console.Write(massiv[i]);
             }                    
-        }        
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task2");
+            /*
+             Задача 2 - Простые числа
+            Определить, является ли число простым. 
+            Простое число: целое положительное число больше 1, 
+            делится без остатка только на себя и 1.
+              */
+            int d;
+            Console.Write("vvedite integer n = ");
+            int number = Int32.Parse(Console.ReadLine());
+            d = 2;
+            while (number % d != 0)
+            {
+                d ++;
+                               
+            }
+            if (d == number)
+            {
+                Console.WriteLine("True, это простое число");
+            }
+            else
+            {
+                Console.WriteLine("False, это не простое число");
+            }
+
+        }
+        static void Task3()
+        {
+            Console.WriteLine("Task3");
+            /*
+            Задача 3 - Палиндром 
+            Определить, является ли строка палиндромом. Например, "а роза упала на лапу Азора".
+            Пробелы предлагаю не учитывать, и большие буквы тоже
+            сделать строку состоящей из маленьких букв:
+            str.ToLower()
+            Заменить символы в строке (в данном случае - пробел на пустую строку)
+            Console.WriteLine(str.Replace(" ", ""));
+              */
+            
+            Console.Write("введите строку = ");
+            string new_str = Console.ReadLine().ToLower().Replace(" ", "");
+                       
+            int dl = new_str.Length;
+            bool p = true;
+            for (int i = 0; i < dl / 2; i++)
+            {
+                 if (new_str[i] != new_str[dl-i-1])
+                {
+                    p = false;      
+                    break;  
+                }              
+            }
+            if (p)
+            {
+                Console.WriteLine("yes, это Палиндром");
+            }
+            else
+            {
+            Console.WriteLine("no, это не палиндром");
+            }
+        
+        }
     }
 }
